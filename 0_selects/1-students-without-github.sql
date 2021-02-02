b0f368827837df01e257a9a802640faa6af8d29d
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-02-01 20:00:50 -0700

    queries

---
+++ 0_selects/1-students-without-github.sql
@@ -0,0 +1,9 @@
+SELECT id, name, email, cohort_id
+
+FROM students
+
+WHERE github is NULL
+
+ORDER BY cohort_id
+
+
---
+++ 1_queries/student-names-per-cohort.sql
@@ -0,0 +1,14 @@
+SELECT id, name
+
+ FROM students
+
+ WHERE cohort_id = 11
+
+ORDER BY name
+
+
+
+
+
+
+
