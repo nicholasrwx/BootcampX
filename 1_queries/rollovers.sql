cb7eb06178f3ea3cd1ae28d2be409db65a4da4ba
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-02-01 23:00:45 -0700

    Join-Queries

---
+++ 1_queries/rollovers.sql
@@ -0,0 +1,13 @@
+SELECT students.name as student_name, cohorts.name as cohort_name,
+
+students.start_date as student_start_date, cohorts.start_date as cohort_start_date
+
+FROM students JOIN cohorts ON cohorts.id = cohort_id
+
+WHERE students.start_date != cohorts.start_date
+
+ORDER BY cohort_start_date;
+
+
+
+
