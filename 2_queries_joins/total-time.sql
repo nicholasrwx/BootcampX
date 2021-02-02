f534fd2c8eff9037199f44ca0b47e744ce1de146
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-02-02 00:03:24 -0700

    More-JOIN-Queries

---
+++ 2_queries_joins/total-time.sql
@@ -0,0 +1,9 @@
+
+SELECT sum(assignment_submissions.duration) as total_duration
+
+FROM assignment_submissions
+
+JOIN students ON students.id = student_id
+
+WHERE students.name = 'Ibrahim Schimmel';
+
