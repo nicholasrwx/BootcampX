e2fc6b37e860fb4744a7ebcee5fff146b0fb1f0d
Nicholas Wiebe <digitaleistence18@gmail.com>
2021-02-05 02:16:22 +0000

    created teachers.js and students.js clients

---
+++ bootcamp_app/students.js
@@ -0,0 +1,45 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+pool.connect();
+
+const coH = process.argv[2];
+let limit = process.argv[3];
+
+pool
+  .query(
+    `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${coH}%'
+LIMIT ${limit || 5};
+`
+  )
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.`
+      );
+    });
+  });
+
+pool.end();
+
+/*
+//pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  console.log(res);
+})
+.catch(err => console.error('query error', err.stack));
+*/
